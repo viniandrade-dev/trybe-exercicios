1d4387dbd8ed298110ab192ba4732b79c00d4669
Vinicius Andrade <vini.fisica.unb@gmail.com>
2022-05-19 14:40:02 -0400

    exercicios sobre and/or/not e switch/case

---
+++ fundamentos/bloco-04-intro-a-js-e-logica-de-programacao/dia-01-js-primeiros-passos/andOrNot.js
@@ -0,0 +1,33 @@
+const currentHour = 2;
+let message;
+
+if (currentHour >= 22){
+    message = 'Não deveríamos comer nada, é hora de dormir.';
+}
+else if (currentHour >=18 && currentHour < 22){
+    message = 'Rango da noite, vamos jantar :D"';
+}
+else if (currentHour >= 14 && currentHour < 18){
+    message = 'Vamos fazer um bolo pro café da tarde?';
+} 
+else if (currentHour >= 11 && currentHour < 14){
+    message = 'Hora do almoço!!!';
+}
+else if (currentHour >= 4 && currentHour < 11){
+    message = 'Hmmm, cheiro de café recém passado'
+}
+else {
+    message = 'estou a mimir';
+}
+
+console.log(message);
+
+let weekDay = 'sábado';
+
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+}
+else {
+    console.log('FINALMENTE, descanso merecido UwU');
+}
+
---
+++ fundamentos/bloco-04-intro-a-js-e-logica-de-programacao/dia-01-js-primeiros-passos/switchcase.js
@@ -0,0 +1,15 @@
+let status = 'reprovado';
+
+switch (status) {
+    case 'aprovado':
+    console.log('Parabéns!');
+    break;
+
+    case 'listado':
+    console.log('Você está na lista de espera');
+    break
+
+    case 'reprovado':
+    console.log('Sinto muito');
+    break
+}
