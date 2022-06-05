927cb11880340aa783b51f56212d721bdb918f84
Vinicius Andrade <vini.fisica.unb@gmail.com>
2022-06-05 00:51:20 -0400

    exercicio 2

--- fundamentos/bloco-05-js-dom-eventos-e-web-storage/dia-03-js-eventos/scriptEventos.js
+++ fundamentos/bloco-05-js-dom-eventos-e-web-storage/dia-03-js-eventos/scriptEventos.js
@@ -23,14 +23,45 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
-const dezDaysList = [
-  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-];
-let daysList = document.getElementById("days");
+function createDaysOfTheMonth() {
+  const dezDaysList = [
+    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
+  let daysList = document.getElementById("days");
 
-for (let i = 0; i < dezDaysList.length; i += 1) {
-  let days = document.createElement("li");
-  days.innerText = dezDaysList[i];
-  daysList.appendChild(days);
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    let days = document.createElement("li");
+    days.innerText = dezDaysList[i];
+    daysList.appendChild(days);
+    days.className = "day";
+    if (
+      dezDaysList[i] === 24 ||
+      dezDaysList[i] === 25 ||
+      dezDaysList[i] === 31
+    ) {
+      days.className = "day holiday";
+    }
+    if (
+      dezDaysList[i] === 4 ||
+      dezDaysList[i] === 11 ||
+      dezDaysList[i] === 18 ||
+      dezDaysList[i] === 25
+    ) {
+      days.className = "day friday";
+    }
+  }
 }
+
+createDaysOfTheMonth();
+
+let buttonContainer = document.querySelector(".buttons-container");
+
+function createHolidayButton(Feriados) {
+  let button = document.createElement("button");
+  button.innerText = "Feriados";
+  button.className = "btn-holiday";
+  buttonContainer.appendChild(button);
+}
+
+createHolidayButton();
